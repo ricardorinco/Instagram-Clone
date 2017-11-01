6dafe48ebce7be866f3a5a9872c41c79ecd8927f
Ricardo Rinco <ricardo.rinco@hotmail.com>
2017-11-01 16:00:34 -0200

    Criação da Timeline

--- api/config/server.js
+++ api/config/server.js
@@ -1,5 +1,7 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
+    fs = require('fs'),
+    multiparty = require('connect-multiparty'),
     mongodb = require('mongodb'),
     objectId = require('mongodb').ObjectId;
 
@@ -7,6 +9,7 @@ var app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(multiparty());
 
 var database = new mongodb.Db(
     'instagram',
@@ -24,6 +27,8 @@ app.get('/', function (req, res) {
 
 // Get
 app.get('/api', function (req, res) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1080');
+
     database.open(function (err, mongoClient) {
         mongoClient.collection('postagens', function (err, collection) {
             collection.find().toArray(function (err, results) {
@@ -37,6 +42,21 @@ app.get('/api', function (req, res) {
             });
 
         });
+    });
+});
+
+// Get
+app.get('/imagens/:imagem', function (req, res) {
+    var img = req.params.imagem;
+
+    fs.readFile(`C:/Users/Ricardo/Desktop/uploads/${img}`, function (err, content) {
+        if (err) {
+            res.status(400).json(err);
+            return;
+        }
+        
+        res.writeHead(200, { 'content-type': 'image/jpg' });
+        res.end(content);
     });
 });
 
@@ -61,19 +81,36 @@ app.get('/api/:id', function (req, res) {
 // Post
 app.post('/api', function (req, res) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:1080');
-    
-    var dados = req.body;
 
-    database.open(function (err, mongoClient) {
-        mongoClient.collection('postagens', function (err, collection) {
-            collection.insert(dados, function (err, results) {
-                if (err) {
-                    res.json('err');
-                } else {
-                    res.status(200).json(results);
-                }
+    var date = new Date();
+    var timestamp = date.getTime();
 
-                mongoClient.close();
+    var originalFileName = `${timestamp}_${req.files.arquivo.originalFilename}`;
+    var pathOrigem = req.files.arquivo.path;
+    var pathDestino = `C:/Users/Ricardo/Desktop/uploads/${originalFileName}`;
+
+    fs.rename(pathOrigem, pathDestino, function (err) {
+        if (err) {
+            res.status(500).json({ error: err });
+            return;
+        }
+
+        var dados = {
+            url_image: originalFileName,
+            titulo: req.body.titulo
+        };
+
+        database.open(function (err, mongoClient) {
+            mongoClient.collection('postagens', function (err, collection) {
+                collection.insert(dados, function (err, results) {
+                    if (err) {
+                        res.json('err');
+                    } else {
+                        res.status(200).json(results);
+                    }
+
+                    mongoClient.close();
+                });
             });
         });
     });
